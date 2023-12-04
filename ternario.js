1ef55992aa2778bb7cfce9ced2481b3fed77807a
Daniel Silveiro Alves <131623522+DanielProjectsDEV@users.noreply.github.com>
2023-12-03 22:34:31 -0300

    Add files via upload

---
+++ ternario.js
@@ -0,0 +1,13 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// if (idadeCliente >= idadeMinima) {
+//    console.log("cerveja")
+// } else {
+//         console.log("suco")
+//     }
+                //condicao                 //true    //false
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
+
+// e uma opcao quando a condicao for simples e curta mas nao seria bom usalo por boas poraticas.
+// se chama operador ternario pois contem 3 operadores em apenas uma linha
